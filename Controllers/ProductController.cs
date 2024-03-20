2f385a5e8b07ee8ff669e7b86c1206996307f321
Gaetano <gaetano@hotmail.com>
2024-03-20 11:59:19 +0100

    primo push

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Veterinario3.Models;
+
+namespace Veterinario3.Controllers
+{
+    public class ProductController : Controller
+    {
+        private VetContext db = new VetContext();
+
+
+        public ActionResult Index()
+        {
+            var companies = db.Companies.ToList();
+            return View(companies);
+        }
+
+        public ActionResult AddProduct()
+        {
+            ViewBag.Companies = db.Companies.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product product, string Tipo, int CompanyId)
+        {
+            if (ModelState.IsValid)
+            {
+                if (CompanyId > 0)
+                {
+                    var company = db.Companies.Find(CompanyId);
+
+                    if (company != null)
+                    {
+                        product.Company = company;
+
+                        if (!string.IsNullOrEmpty(Tipo))
+                        {
+                            product.Tipologia = Tipo;
+                        }
+
+                        db.Products.Add(product);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "La compagnia selezionata non esiste.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Seleziona una compagnia valida.");
+                }
+            }
+
+            ViewBag.Companies = db.Companies.ToList();
+            return View(product);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult IncreaseQuantity(int productId)
+        {
+            var product = db.Products.Find(productId);
+            if (product != null)
+            {
+                product.Quantità++;
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
